d62d36c3c40bfce0bd965c892b0b8682b673b75f
OlivierBoisvert <olivierboisv@gmail.com>
2026-02-26 11:07:42 -0500

    Ajout Ressources

---
+++ app/Http/Resources/RentalResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RentalResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'total_price' => $this->total_price,
+            'user_id' => $this->user_id,
+            'equipment_id' => $this->equipment_id
+        ];
+    }
+}
